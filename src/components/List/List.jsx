a6dc505125d8fe72d2adb2ad745f354a1e084fc8
Nikjee <boss.bold@mail.ru>
2021-07-23 18:23:18 +0300

    Badge

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import classNames from 'classnames'
+import Badge from '../Badge/Badge'
+
 import './List.scss'
 
-function List({ items, isRemovable }) {
+function List({ items, isRemovable, onClick }) {
   return (
-    <ul className="list">
+    <ul onClick={onClick} className="list">
       {items.map((item) => (
         <li className={classNames(item.className, { active: item.active })}>
-          <i>
-            {item.icon ? (
-              item.icon
-            ) : (
-              <i className={`badge badge--${item.color}`}></i>
-            )}
-          </i>
+          <i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
           <span>{item.name}</span>
         </li>
       ))}
