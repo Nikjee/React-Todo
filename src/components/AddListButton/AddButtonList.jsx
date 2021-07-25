129ea538e9f5e24a82e86a9ca59b5c5a9d6b5af6
Nikjee <boss.bold@mail.ru>
2021-07-25 18:34:25 +0300

    started tasks component

--- src/components/AddListButton/AddButtonList.jsx
+++ src/components/AddListButton/AddButtonList.jsx
@@ -6,9 +6,30 @@ import closeSvg from '../../assets/img/close.svg'
 
 import './AddListButton.scss'
 
-function AddListButton({ colors }) {
-  const [visiblePopup, setVisiblePopup] = React.useState(true)
+function AddListButton({ colors, onAdd }) {
+  const [visiblePopup, setVisiblePopup] = React.useState(false)
   const [selectedColor, selectColor] = React.useState(colors[0].id)
+  const [inputValue, setInputValue] = React.useState('')
+
+  const onClose = () => {
+    setVisiblePopup(false)
+    selectColor(colors[0].id)
+    setInputValue('')
+  }
+
+  const addList = () => {
+    if (!inputValue) {
+      alert('Введите название списка')
+      return
+    }
+    const color = colors.filter((c) => c.id === selectedColor)[0].name
+    onAdd({
+      id: 1,
+      name: inputValue,
+      color,
+    })
+    onClose()
+  }
 
   return (
     <div className="add-list">
@@ -48,12 +69,18 @@ function AddListButton({ colors }) {
       {visiblePopup && (
         <div className="add-list__popup">
           <img
-            onClick={() => setVisiblePopup(false)}
+            onClick={onClose}
             src={closeSvg}
             alt="Close button"
             className="add-list__popup-close-btn"
           />
-          <input className="field" type="text" placeholder="Название списка" />
+          <input
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            className="field"
+            type="text"
+            placeholder="Название списка"
+          />
           <div className="add-list__popup-colors">
             {colors.map((color) => (
               <Badge
@@ -64,7 +91,9 @@ function AddListButton({ colors }) {
               />
             ))}
           </div>
-          <button className="button">Добавить</button>
+          <button onClick={addList} className="button">
+            Добавить
+          </button>
         </div>
       )}
     </div>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,15 +2,30 @@ import React from 'react'
 import classNames from 'classnames'
 import Badge from '../Badge/Badge'
 
+import removeSvg from '../../assets/img/remove.svg'
+
 import './List.scss'
 
-function List({ items, isRemovable, onClick }) {
+function List({ items, isRemovable, onClick, onRemove }) {
+  const removeList = (item) => {
+    if (window.confirm('Вы действительно хотите удалить список?'))
+      onRemove(item)
+  }
+
   return (
     <ul onClick={onClick} className="list">
       {items.map((item) => (
         <li className={classNames(item.className, { active: item.active })}>
           <i>{item.icon ? item.icon : <Badge color={item.color} />}</i>
           <span>{item.name}</span>
+          {isRemovable && (
+            <img
+              className="list__remove-icon"
+              src={removeSvg}
+              alt="remove"
+              onClick={() => removeList(item)}
+            ></img>
+          )}
         </li>
       ))}
     </ul>
