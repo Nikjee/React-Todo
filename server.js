983f847730bc3113cd9e33e01a830b51a7d55b68
Nikjee <boss.bold@mail.ru>
2022-09-15 17:59:10 +0300

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const data = require('./public/db.json')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('./public/db.json')
+const router = jsonServer.router(data)
 const middlewares = jsonServer.defaults({
   static: './build',
 })
